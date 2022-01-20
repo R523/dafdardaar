04719c3d03cdfa56cd63c222cc296779e616a0a6
Parham Alvani <parham.alvani@gmail.com>
2022-01-20 08:34:16 +0330

    feat: improve board mqtt connection

--- internal/board/board.go
+++ internal/board/board.go
@@ -28,7 +28,7 @@ type Config struct {
 func New(ctx context.Context, cfg Config, logger *zap.Logger) (Board, error) {
 	serverURL, err := url.Parse(cfg.ServerURL)
 	if err != nil {
-		return Board{}, err
+		return Board{}, fmt.Errorf("invalid server url %w", err)
 	}
 
 	cliCfg := autopaho.ClientConfig{
@@ -36,21 +36,29 @@ func New(ctx context.Context, cfg Config, logger *zap.Logger) (Board, error) {
 		KeepAlive:         cfg.KeepAlive,
 		ConnectRetryDelay: cfg.ConnectRetryDelay,
 		OnConnectionUp: func(cm *autopaho.ConnectionManager, connAck *paho.Connack) {
-			fmt.Println("mqtt connection up")
+			logger.Info("mqtt connection up")
+
 			if _, err := cm.Subscribe(context.Background(), &paho.Subscribe{
 				Subscriptions: map[string]paho.SubscribeOptions{},
+				Properties:    nil,
 			}); err != nil {
-				fmt.Printf("failed to subscribe (%s). This is likely to mean no messages will be received.", err)
+				logger.Error("failed to subscribe. this is likely to mean no messages will be received.", zap.Error(err))
+
 				return
 			}
-			fmt.Println("mqtt subscription made")
+
+			logger.Info("mqtt subscription made")
 		},
-		OnConnectError: func(err error) { fmt.Printf("error whilst attempting connection: %s\n", err) },
+		OnConnectError: func(err error) {
+			logger.Error("error whilst attempting connection", zap.Error(err))
+		},
 		ClientConfig: paho.ClientConfig{
 			ClientID: "daftardaar",
 			Router: paho.NewSingleHandlerRouter(func(m *paho.Publish) {
 			}),
-			OnClientError: func(err error) { fmt.Printf("server requested disconnect: %s\n", err) },
+			OnClientError: func(err error) {
+				fmt.Printf("server requested disconnect: %s\n", err)
+			},
 			OnServerDisconnect: func(d *paho.Disconnect) {
 				if d.Properties != nil {
 					fmt.Printf("server requested disconnect: %s\n", d.Properties.ReasonString)
@@ -61,17 +69,12 @@ func New(ctx context.Context, cfg Config, logger *zap.Logger) (Board, error) {
 		},
 	}
 
-	//
-	// Connect to the broker
-	//
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	cm, err := autopaho.NewConnection(ctx, cliCfg)
+	connectionManager, err := autopaho.NewConnection(ctx, cliCfg)
 	if err != nil {
-		return Board{}, err
+		return Board{}, fmt.Errorf("broker connection failed %w", err)
 	}
 
 	return Board{
-		Connection: cm,
+		Connection: connectionManager,
 	}, nil
 }
