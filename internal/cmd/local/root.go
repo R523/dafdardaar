920eae6817ad408ac9cc553c3dd0dc2e2e6c4ad4
Parham Alvani <parham.alvani@gmail.com>
2022-01-25 07:59:49 +0330

    feat: setup board on local server

--- internal/cmd/local/root.go
+++ internal/cmd/local/root.go
@@ -1,30 +1,47 @@
 package local
 
 import (
+	"context"
 	"os"
 
+	"github.com/r523/dafdardaar/internal/board"
 	"github.com/r523/dafdardaar/internal/config"
+	"github.com/r523/dafdardaar/internal/logger"
 	"github.com/spf13/cobra"
+	"go.uber.org/zap"
 )
 
 // ExitFailure status code.
 const ExitFailure = 1
 
+func main(ctx context.Context, cfg config.Config, logger *zap.Logger) {
+	board, err := board.New(ctx, cfg.BoardConnection, logger)
+	if err != nil {
+		logger.Fatal("board initiation failed", zap.Error(err))
+	}
+
+	<-board.Connection.Done()
+	logger.Info("up and running connection to mqtt broker")
+}
+
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	_ = config.New()
+	cfg := config.New()
 
-	// logger := logger.New(cfg.Logger)
+	logger := logger.New(cfg.Logger)
 
 	// nolint: exhaustivestruct
 	root := &cobra.Command{
-		Use:   "saf",
-		Short: "Queue with NATS Jetstream to remove all the erlangs from cloud",
+		Use:   "dafdardaar-local",
+		Short: "Office Local Controller",
+		Run: func(cmd *cobra.Command, args []string) {
+			main(cmd.Context(), cfg, logger)
+		},
 	}
 
 	if err := root.Execute(); err != nil {
-		// logger.Error("failed to execute root command", zap.Error(err))
+		logger.Error("failed to execute root command", zap.Error(err))
 
 		os.Exit(ExitFailure)
 	}
